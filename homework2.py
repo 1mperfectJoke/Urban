488dd512e82808d4149dda706e8333bc718eb0ac
D K <kireeffdiman@ya.ru>
2024-07-02 21:47:30 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+nmbr_of_hw = '12'
+nmbr_of_hrs = '1.5'
+crsName = 'Python'
+timeFor1Task = '0.125'
+print('Всего задач: ' + nmbr_of_hw + ',', 'затрачено часов: ' + nmbr_of_hrs + ',',
+      'курс: ' + crsName + ',', 'среднее время выполнения: ' + timeFor1Task + '.')
