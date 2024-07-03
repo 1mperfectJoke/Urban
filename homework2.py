d78314a67e01b5f8cee06b2a4b866a594f643bf4
D K <kireeffdiman@ya.ru>
2024-07-03 17:38:13 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,6 +1,6 @@
-nmbr_of_hw = '12'
-nmbr_of_hrs = '1.5'
+nmbr_of_hw = 12
+nmbr_of_hrs = 1.5
 crsName = 'Python'
-timeFor1Task = '0.125'
-print('Всего задач: ' + nmbr_of_hw + ',', 'затрачено часов: ' + nmbr_of_hrs + ',',
-      'курс: ' + crsName + ',', 'среднее время выполнения: ' + timeFor1Task + '.')
+timeFor1Task = nmbr_of_hrs/nmbr_of_hw
+print('Всего задач:',nmbr_of_hw,',', 'затрачено часов:',nmbr_of_hrs,',',
+      'курс:',crsName,',', 'среднее время выполнения:',timeFor1Task,'.')
