c1e5ccff3fea941e1040eb5274b1d8f67056b811
D K <kireeffdiman@ya.ru>
2024-07-04 21:36:10 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+    #Организация программы
+my_string = input('Введите слово: ')
+print('В данном слове', len(my_string), 'символ(а/ов).')
+
+    #Работа с методами строк
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
