e8f63f75b1f5707f064fa99efa70a46d54b7a4b4
D K <kireeffdiman@ya.ru>
2024-07-07 19:23:00 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,6 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+s = sorted(students)
+g = [sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]), sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4])]
+gpa = {s[0]:g[0], s[1]:g[1], s[2]:g[2], s[3]:g[3], s[4]:g[4]}
+print(gpa)
